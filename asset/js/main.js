1bcdcf6173f246704f47abfb2fa4362457967e7f
Developer 8ᵗʰ <developer.8th@gmail.com>
2022-10-04 21:27:58 +0700

    Initial commit

---
+++ asset/js/main.js
@@ -0,0 +1,142 @@
+    
+    //TITLE AWAL
+    var title1 = "<h3>Selamat Datang</h3>";
+    var title2 = "<h1>Main Suwit</h1>";
+    var title3 = "<h3>Oleh Developer 8ᵗʰ</h3>";
+    var notif = document.getElementsByClassName("notif");
+    notif[0].innerHTML=title1+title2+title3;
+
+    //JIKA TOMBOL SUWIT DITEKAN
+    function play(e) {
+
+        var audio = new Audio('asset/audio/click.wav');
+        audio.play();
+
+        //KOSONGKAN TITLE
+        document.getElementsByClassName("notif")[0].innerHTML = "";
+
+        var notif = document.getElementsByClassName("notif");
+        var pilihanpemain = document.getElementsByClassName("pilihanpemain");
+        var pilihankomputer = document.getElementsByClassName("pilihankomputer");
+
+        //AMBIL DATA SKOR TERAKHIR
+        var el_skorpemain = document.getElementsByClassName("skorpemain");
+        var el_skorkomputer = document.getElementsByClassName("skorkomputer")
+        var skorpemain = parseInt(el_skorpemain[0].textContent);
+        var skorkomputer = parseInt(el_skorkomputer[0].textContent);
+
+        //ATURAN
+        const aturan = [
+            [
+                "seri",
+                "menang",
+                "kalah"
+            ],
+            [
+                "kalah",
+                "seri",
+                "menang"
+            ],
+            [
+                "menang",
+                "kalah",
+                "seri"
+            ]
+        ];
+
+        //OPSI PILIHAN
+        const opsi = ["Batu", "Gunting", "Kertas"];
+
+        //MENENTUKAN PILIHAN COM
+        var random = Math.floor(Math.random()*opsi.length);
+
+        //PILIHAN
+        var opsi_pemain = opsi[e];
+        var opsi_com = opsi[random];
+
+        var title1 = "<h3>" + opsi[e] + " melawan " + opsi[random] + "</h3>";
+        if (aturan[e][random] == "seri") {
+            var title2 = "<h1 style='color:#cbd1ca'>SERI</h1>";
+        } else if (aturan[e][random] == "kalah") {
+            var title2 = "<h1 style='color:#dd2222'>KALAH</h1>";
+            skorkomputer += 1;
+        } else {
+            var title2 = "<h1 style='color:#22dd39'>MENANG</h1>";
+            skorpemain += 1;
+        }
+
+        pilihankomputer[0].innerHTML="<img style='transform:rotate(270deg);' src='asset/pic/"+opsi_com+".png'><h3 style='color:red'>COM</h3>";
+        pilihanpemain[0].innerHTML="<img style='transform:rotate(90deg) scaleX(-1)' src='asset/pic/"+opsi_pemain+".png'><h3 style='color:blue'>KAMU</h3>";
+        notif[0].innerHTML=title2;
+
+        el_skorkomputer[0].textContent = skorkomputer;
+        el_skorpemain[0].textContent = skorpemain;
+
+    }
+
+    function yoona(choose){
+
+        const arr = [];
+        const arr_filter = [];
+        var num = 0;
+        var hitung = 0;
+        var b = "";  
+        
+        //total storage
+        var totalstore = localStorage.length-1;
+        
+        //hitung step
+        for(var i = 0; i <= totalstore; i++){
+            if(localStorage.key(i).match("step")){
+                num++;
+            }
+        }  
+        
+        //totalstep - 1 (array_mode)
+        var totalstep = num - 1 ;
+        
+        if(totalstep<9){
+        
+            //TAMBAH STEP
+            var nextstep = totalstep + 1;
+            localStorage.setItem("step"+nextstep,choose);
+        
+        }else{
+        
+            hitung=0;
+            for(i=0;i<=totalstep;i++){
+                arr[i]=localStorage.getItem("step"+i); 
+                if(localStorage.getItem("step"+i)==choose){
+                    var a = i+1;
+                    if(localStorage.getItem("step"+a)==b){
+                        hitung++;
+                    }else{
+                        if(hitung!=0){
+                            arr_filter = [localStorage.getItem("step"+a),hitung];
+                        }
+                        b = localStorage.getItem("step"+a);
+                        hitung=1;
+                        
+                    }
+                }
+            }  	
+            
+            //TIMPA STEP
+            for(i=0;i<=totalstep;i++){
+                
+                if(i<totalstep){
+                
+                    localStorage.setItem("step"+i,arr[i+1]);
+                
+                }else{
+                
+                    localStorage.setItem("step"+totalstep, choose);
+                
+                }
+                
+            }
+        
+        }
+
+
+    }
---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/css/main.css?version=<?= filemtime("asset/css/main.css") ?>">
+    <title>Suwit</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="fixed-top">
+            <div class="papanskor">
+                <div class="pemain">
+                    <div class="skorpemain">0</div>
+                    <label for="skor">Kamu</label>
+                </div>
+                <div class="versus">
+                    <label for="versus">VS</label>
+                </div>
+                <div class="komputer">
+                    <div class="skorkomputer">0</div>
+                    <label for="skor">COM</label>
+                </div>
+            </div>
+        </div>
+
+        <div class="onthecenter">
+            <div class="hasil">
+                <div class="notif"></div>
+                <div class="pilihan">
+                    <div class="pilihanpemain"></div>
+                    <div class="pilihankomputer"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="fixed-bottom">
+            <div class="list-tombol">
+                <!-- Batu / Ibu Jari -->
+                <button id="0" onclick="play(this.id)">
+                    <img src="asset/pic/Batu.png" alt="batu">
+                </button>
+                <!-- Gunting / Telunjuk -->
+                <button id="1" onclick="play(this.id)">
+                    <img src="asset/pic/Gunting.png" alt="gunting">
+                </button>
+                <!-- Kertas / Kelingking -->
+                <button id="2" onclick="play(this.id)">
+                    <img src="asset/pic/Kertas.png" alt="kertas">
+                </button>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+<script src="asset/js/main.js"></script>
+
+</html>
